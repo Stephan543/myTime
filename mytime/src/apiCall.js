814ce799a6ece44ab34cee07b43dc58a91482e75
Stephan Iskander <stephan.iskander@gmail.com>
2021-03-22 10:34:22 -0400

    Code Clean Up

--- mytime/src/apiCall.js
+++ mytime/src/apiCall.js
@@ -1,29 +1,23 @@
 const { google } = require("googleapis");
 const { OAuth2 } = google.auth;
-
 const sensitiveInfo = require("./gCalApiAccess.js").sensitiveInfo;
 const allCalendars = require("./gCalApiAccess.js").allCalendars;
-
 const oAuth2Client = new OAuth2(
   sensitiveInfo.clientId, // Client ID - from google developer API Auth
   sensitiveInfo.clientSecret // Client Secret - from google developer API Auth
 );
-
+const calendar = google.calendar({ version: "v3", auth: oAuth2Client }); //Creating new calendar instance
 oAuth2Client.setCredentials({
   refresh_token: sensitiveInfo.refreshToken, // refresh token - from google developer API Auth
 });
 
-const calendar = google.calendar({ version: "v3", auth: oAuth2Client }); //Creating new calendar instance
-
-var eventStartTime = new Date();
-eventStartTime.setDate(eventStartTime.getDate());
-// console.log(eventStartTime);
-
+// Search time initialization
+var eventStartTime = new Date(); 
+  eventStartTime.setDate(eventStartTime.getDate());
 var eventEndTime = new Date();
-eventEndTime.setDate(eventEndTime.getDate() + 15);
-// eventEndTime.setTime(eventEndTime.getTime() + 604800000);
+  eventEndTime.setDate(eventEndTime.getDate() + 15);
 
-// Check for conflicts
+// Google calendar api query request
 calendar.freebusy.query(
   {
     resource: {
@@ -62,82 +56,60 @@ calendar.freebusy.query(
       }
       return 0;
     });
-    // console.log(newArr); //log a sorted array for all Calendars
-    // return freeSlots(eventStartTime, res.data.calendars[allCalendars[4].id].busy)
 
-    const timeInt = 1800000*2; // Meeting Minutes (30 min) in miliseconds
+    var timeInt = 1800000; // Meeting Minutes (30 min) in 
+    timeInt *= 2; // Convert to 1hr meeting minutes
     var availableTimeArr = []; //Free Time array to show
-    var searchStartTime = new Date(),
-      searchEndTime = new Date();
-
-    searchStartTime.setDate(searchStartTime.getDate());
-    // searchEndTime.setTime(searchEndTime.getTime() + timeInt);
-    // searchStartTime.setHours(8)
-
-    const mSecondCoeff = 1000 * 60 * 60 * 24;
-    const searchDays = (eventEndTime - eventStartTime);
+    var searchStartTime = new Date();
+      searchStartTime.setDate(searchStartTime.getDate());
     var viewTimeEnd = new Date();
+
     // Recursive Function to create new available time slots for each day
     function freeDaySearch(viewTime, arrIndex) {
-      // console.log(arrIndex + '+')
       var viewTimeStart = new Date(viewTime);
 
-      // console.log(eventEndTime.toString());
-
-      if ((viewTimeStart.getTime() )>= eventEndTime.getTime() || arrIndex > newArr.length - 1) {
-        return; //Cancel the recursion given my preffered work hours
+      if (
+        viewTimeStart.getTime() >= eventEndTime.getTime() ||
+        arrIndex > newArr.length - 1
+      ) {
+        return; //Exit Recursion if the search time goes beyond the query end or when the array index goes beyond the real size
       }
 
-      var testArrDateStart = new Date(newArr[arrIndex].start);
-      var testArrDateEnd = new Date(newArr[arrIndex].end);
+      var testArrDateStart = new Date(newArr[arrIndex].start); // Initilize the busy array's start time
 
-      if(viewTimeStart.getHours() >= 20 || viewTimeStart.getHours()==0){ // Check for end of day boundary
-        if(viewTimeStart.getHours() !== 0){ // Catches all day events
+      if (viewTimeStart.getHours() >= 20 || viewTimeStart.getHours() == 0) {
+        // Check for end of day boundary
+        if (viewTimeStart.getHours() !== 0) {
+          // Catches all-day events
           viewTimeStart.setDate(viewTimeStart.getDate() + 1); // Set search time to next day if at the end of the day
         }
-        viewTimeStart.setHours(8) //Set it to search time to my preferred start time ... 8am
+        viewTimeStart.setHours(8); //Set it to search time to my preferred start time ... 8am
         viewTimeStart.setTime(viewTimeStart.getTime() - timeInt); //correct day search my time int 8:30 vs 8am
-      return freeDaySearch(viewTimeStart, arrIndex); // New Day search recursion
-
+        return freeDaySearch(viewTimeStart, arrIndex); // New Day search recursion
       }
-      console.log(viewTimeStart.getDate() < testArrDateStart.getDate())
-      console.log(viewTimeStart.getDate().toString())
-      console.log(testArrDateStart.getDate().toString())
 
-      if (viewTimeStart.getTime() <= testArrDateStart.getTime()) { 
-        if (viewTimeStart.getTime() + timeInt <= testArrDateStart.getTime() && viewTimeStart.getHours() !==0) {
+      if (viewTimeStart.getTime() <= testArrDateStart.getTime()) {
+        if (
+          viewTimeStart.getTime() + timeInt <= testArrDateStart.getTime() &&
+          viewTimeStart.getHours() !== 0
+        ) {
           viewTimeEnd.setTime(viewTimeStart.getTime() + timeInt);
           availableTimeArr.push({
             start: viewTimeStart.toString(),
             end: viewTimeEnd.toString(),
           });
-  
-            console.log(viewTimeStart.toString())
-
-            console.log(arrIndex + 'B');
           return freeDaySearch(viewTimeEnd, arrIndex); // Recursion after new object creation
         } else {
-          console.log(viewTimeStart.toString())
-
-          console.log(arrIndex + 'C');
-
           return freeDaySearch(newArr[arrIndex].end, arrIndex + 1); // Recursion if there is not enough space in a possible window
         }
-      } else  {
-        console.log(arrIndex + 'D');
-
+      } else {
         return freeDaySearch(newArr[arrIndex].end, arrIndex + 1); // recursion if the start time is after the new Array (meetings) Obj
       }
-     
     }
 
     freeDaySearch(searchStartTime, 0);
-    // console.log(newArr);
     console.log(availableTimeArr);
-    // console.log(newArr.length);
-    // console.log(newArr);
   }
-
 );
 
 // Event Creation
